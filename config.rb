db827a353080126a82b45e49c8d5479284dd5eff
Erik Person <eperson6@gmail.com>
2012-04-02 16:11:43 -0500

    Getting raw bits up and running

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/hudl/"
+http_images_path = "/hudl/images"
+http_fonts_path = "/hudl/fonts"
+css_dir = "public/hudl/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
---
+++ source/_posts/2012-04-02-linq-to-objective-c.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "Linq to Objective-C"
+date: 2012-04-02 13:24
+comments: true
+categories: 
+---
