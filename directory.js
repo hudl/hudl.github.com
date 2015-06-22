2c31919f239754b0e0ca2d717ed21222d01e1840
Chris Roberson <chrisronline@gmail.com>
2015-06-22 16:54:32 -0500

    Web font stub support

--- directory.js
+++ directory.js
@@ -16,3 +16,16 @@
     return t;
   }(document, "script", "twitter-wjs"));
 })();
+
+// Web fonts
+(function(document) {
+  WebFontConfig = {
+    custom: {
+      families: ['Trade Gothic'],
+      urls: ['fonts.css']
+    }
+  };
+  var wf = document.createElement('script'), s = document.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+  s.parentNode.insertBefore(wf, s);
+})(document);
