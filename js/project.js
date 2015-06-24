81ebae52bfceb18d9a33d55d97d52dfc4bc056d6
Chris Roberson <chrisronline@gmail.com>
2015-06-24 17:12:39 -0500

    Fix project link issue

--- js/project.js
+++ js/project.js
@@ -1,12 +1,12 @@
 (function($) {
-  $('.project').each(function() {
+  $('.project-left, .project-right').each(function() {
     var link = $(this).find('a[class*="project__link"]');
     var href = link.attr('href');
     link.remove();
     $(this).attr('data-href', href);
   });
 
-  $('.project').on('click', function() {
+  $('.project-left, .project-right').on('click', function() {
     var href = $(this).data('href');
     if (href) {
       ga && ga('send', 'event', 'outbound', 'click', href, 0);
